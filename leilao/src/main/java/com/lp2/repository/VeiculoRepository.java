e81356b41c9a0f50df0c7c444bf7088d8140205b
Alan Araujo Rodrigues <alanaraujotrs@gmail.com>
2023-11-09 17:18:38 -0300

    Melhoria no metodo de buscar um produto pelo id do leilao

--- leilao/src/main/java/com/lp2/repository/VeiculoRepository.java
+++ leilao/src/main/java/com/lp2/repository/VeiculoRepository.java
@@ -1,9 +1,15 @@
 package com.lp2.repository;
 
+import com.lp2.model.DispositivoInformatica;
 import com.lp2.model.Veiculo;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface VeiculoRepository<T extends Veiculo> extends JpaRepository<T, Long> {
+
+    Optional<Veiculo> findByIdAndLeilaoId(Long idVeiculo, Long idLeilao);
+
 }
